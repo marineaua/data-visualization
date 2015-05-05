9c6852a5f74e9119a76718042a122f8f82a54cad
Alex <marineaua@sou.edu>
2015-05-04 20:13:48 -0700

    removed scroll bar

--- DNA/src/ProccesingSketch.java
+++ DNA/src/ProccesingSketch.java
@@ -25,7 +25,6 @@ public class ProccesingSketch extends PApplet {
 	private static int			height				= 600;
 	private static String		newFilePath 		= Scraper.filePath.substring(0, Scraper.filePath.length()-4);
 	private static String		thePath				= "C:/Users/alexm_000/Documents/data-visualization/DNA/src/files/" + Scraper.tumblrName + ".txt";
-	private 	   HScrollbar 	hs1;
 	int 		   centerX 							= 0;
 	int 		   centerY 							= 0;
 	int 		   offsetX 							= 0;
@@ -63,7 +62,6 @@ public class ProccesingSketch extends PApplet {
 			centerX = mouseX-offsetX;
 			centerY = mouseY-offsetY;
 		}
-		
 		translate(centerX,centerY);
 		scale(zoom);
 		
@@ -82,7 +80,6 @@ public class ProccesingSketch extends PApplet {
 		else if(MyGUI.getDrawingType() == 3)
 		{
 			drawLineGraph();
-			hs1 = new HScrollbar(0, height/2-8, width, 16, 16);
 		}
 	}
 	
